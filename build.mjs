442cd06aa1987c07d820a035597988f2bf0f92dd
yukkie8058 <yukkie8058@gmail.com>
2025-03-26 00:58:52 +0900

    Configure deployment to GitHub Pages

--- build.mjs
+++ build.mjs
@@ -2,12 +2,19 @@ import 'zx/globals'
 import fs from 'fs'
 import path from 'path'
 
+const base = 'https://yukkie8058.github.io/slides/'
 const slidesDir = './slides'
 const distDir = './dist'
+
+fs.rmSync(distDir, { recursive: true, force: true })
+
 fs.readdirSync(slidesDir).forEach(file => {
   const fileName = path.basename(file)
   const slide = path.join(slidesDir, fileName, `${fileName}.md`)
   if (fs.existsSync(slide)) {
-    $`slidev build ${slide} --out ${path.resolve(distDir, fileName)}`
+    $`slidev build ${slide} \
+      --out ${path.resolve(distDir, fileName)} \
+      --base ${path.join(base, fileName)} \
+    `
   }
 });
