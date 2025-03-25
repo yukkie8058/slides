8303678a9a3691e16877cae9edbfc932a3e875d0
yukkie8058 <yukkie8058@gmail.com>
2025-03-26 00:03:28 +0900

    Add environment variable transformer for things I want to keep secret

---
+++ setup/transformers.ts
@@ -0,0 +1,29 @@
+import * as dotenvx from '@dotenvx/dotenvx'
+import { defineTransformersSetup, MarkdownTransformContext, MarkdownTransformer } from '@slidev/types'
+
+dotenvx.config({ path: ['.env', '.env.local'] })
+
+export default defineTransformersSetup(() => {
+  return {
+    pre: [],
+    preCodeblock: [
+      envTransformer(),
+    ],
+    postCodeblock: [],
+    post: [],
+  }
+})
+
+function envTransformer(): MarkdownTransformer {
+  return (ctx: MarkdownTransformContext) => {
+    ctx.s.replaceAll(/%([^ ]+?)%/g, (str, key) => {
+      if (typeof key !== 'string') throw new TypeError()
+      if (process.env[key] != null) {
+        return process.env[key]
+      } else {
+        console.warn(`Undefined environment variable: ${key}`)
+        return str
+      }
+    })
+  }
+}
